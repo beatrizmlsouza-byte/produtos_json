85eaada1ca17e8b91fb0640ffd69fcb5ca1006b9
Bia <beatrizmlsouza@gmail.com>
2026-05-09 19:53:04 -0300

    first commit

--- bd/servicos.js
+++ bd/servicos.js
@@ -1,3 +1,4 @@
+import { act } from "react";
 import { produtos } from "./bd.js";
 
 function listarProdutos(){
@@ -11,4 +12,47 @@ function listarPorCategoria(categoria){
     return produtosFiltrados
 }
 
-export {listarProdutos, listarPorCategoria}
+function contarDinheiroEstoque(){
+    let total = produtos.reduce((acumulador, produto) => {
+        return acumulador + (produto.preco * produto.quantidade);
+    }, 0);
+    return total;
+}
+
+function buscarProdutoPorId(id){
+    return produtos.find(produto => produto.id === id)
+}
+
+function buscarProdutoPorNome(nome){
+    return produtos.find(produto => produto.nome === nome);
+}
+
+function buscarQuantidadeMenor10(){
+    return produtos.filter(produto => produto.quantidade < 10);
+}
+
+function top5ItensMaisCaros(){
+    const produtosOrdenados = [...produtos].sort((a, b) => b.preco - a.preco);
+    return produtosOrdenados.slice(0, 5);
+}   
+
+function maiorPreco(){
+    return produtos.reduce((maior, produto) => {
+        return produto.preco > maior.preco ? produto : maior;
+    }, produtos[0]);
+}
+
+function dinheiroPorCategoria(){
+    return produtos.reduce((categorias, produto) => {
+        if (!categorias[produto.categoria]) {
+            categorias[produto.categoria] = 0;
+        }
+        categorias[produto.categoria] += produto.preco * produto.estoque;
+        return categorias;
+    }, {});
+}
+
+
+
+
+export {listarProdutos, listarPorCategoria, contarDinheiroEstoque, buscarProdutoPorId, buscarProdutoPorNome, buscarQuantidadeMenor10, top5ItensMaisCaros, maiorPreco, dinheiroPorCategoria}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { listarProdutos, listarPorCategoria } from './bd/servicos.js'
+import { listarProdutos, listarPorCategoria, contarDinheiroEstoque, buscarProdutoPorId, buscarProdutoPorNome, buscarQuantidadeMenor10, top5ItensMaisCaros, maiorPreco, dinheiroPorCategoria } from './bd/servicos.js'
 
 
 const app = express()
@@ -18,6 +18,43 @@ app.get('/produtosCategoria/:categoria', (req, res) => {
    let {categoria} = req.params
    let produtosFiltrados = listarPorCategoria(categoria)
    res.json(produtosFiltrados)
+})
+
+app.get('/contarDinheiroEstoque', (req, res) => {
+   let total = contarDinheiroEstoque()
+   res.json(total)
+})
+
+app.get('/buscarProdutoPorId/:id', (req, res) => {
+   let {id} = req.params
+   let produto = buscarProdutoPorId(Number(id))
+   res.json(produto)
+})
+
+app.get('/buscarProdutoPorNome/:nome', (req, res) => {
+   let {nome} = req.params
+   let produto = buscarProdutoPorNome(nome)
+   res.json(produto)
+})
+
+app.get('/buscarQuantidadeMenor10', (req, res) => {
+   let produtos = buscarQuantidadeMenor10()
+   res.json(produtos)
+})
+
+app.get('/top5ItensMaisCaros', (req, res) => {
+   let produtos = top5ItensMaisCaros()
+   res.json(produtos)
+})
+
+app.get('/maiorPreco', (req, res) => {
+   let produto = maiorPreco()
+   res.json(produto)
+})
+
+app.get('/dinheiroPorCategoria', (req, res) => {
+   let categorias = dinheiroPorCategoria()
+   res.json(categorias)
 })
 
 app.listen(3000, () => {
